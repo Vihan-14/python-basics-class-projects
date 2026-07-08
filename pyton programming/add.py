aa1796dec776a2ec7ef8de208b7cdb2600151436
Vihan-14 <arun.07ib@gmail.com>
2026-07-08 20:40:49 +0530

    Add files via upload

---
+++ pyton programming/add.py
@@ -0,0 +1,5 @@
+# this is adding numbers
+a=34
+b=23
+c=a+b
+print("this is a sum of a + b" , c )
---
+++ pyton programming/print.py
@@ -0,0 +1,6 @@
+print("vihan")
+print(1004)
+print(4+3 )
+a=7
+print(a)
+print("this is a pyton code " , end=".")
