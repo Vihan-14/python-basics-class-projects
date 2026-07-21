523474114bbd2c7cc9f049d4a7f78cbee764debf
Vihan-14 <arun.07ib@gmail.com>
2026-07-21 19:48:31 +0530

    Add files via upload

---
+++ conditional statement/shoes.py
@@ -0,0 +1,8 @@
+puddle = str(input("are the puddles on the ground ?(yes or no)"))
+if puddle == "yes" :
+    shoes = "boots"
+    print("the ground is wet , please wear ",shoes)
+else :
+    shoes = "sneakers"
+    print("good to go. you can wear ",shoes )
+
---
+++ conditional statement/temprature.py
@@ -0,0 +1,8 @@
+temp = int(input("Enter today's temperature in Celsius:"))
+
+if temp <=20:
+    print("jackets")
+elif temp >=21 and temp <=30:
+    print("normal outfit")
+elif temp >=31 and temp <=40:
+    print("cotton outfit")
