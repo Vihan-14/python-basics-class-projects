f760ede0ec4f0740e4feba98e0d634237c939e44
Vihan-14 <arun.07ib@gmail.com>
2026-07-24 13:53:32 +0530

    Add files via upload

---
+++ python opreater 2/logical opreater.py
@@ -0,0 +1,35 @@
+# and opreater
+weather = "rainy"
+homework = "yes"
+
+if weather == "sunny" and homework == "yes" :
+    print("you are allowed to go out to play") 
+else :
+    print("you are not allowed to go out to play")  
+
+# or opreater
+
+day = "Sunday"
+if day == "Sunday" or day == "Saturday":
+    print("it is a weekend")
+else :
+    print("it is a weekend")
+
+# not opreater
+a = 10
+b = 10
+
+if a != b:
+    print("value of a and b is diffrent")
+else : 
+    print("value of a and b is same")
+
+
+
+    
+
+
+
+
+
+ 
